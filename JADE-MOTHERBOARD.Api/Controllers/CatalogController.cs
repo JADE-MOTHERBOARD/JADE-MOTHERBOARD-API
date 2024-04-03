4446b86d29f0e5c8a6e87c0c130d9223dd2aca05
Alina Mueez <mueez.1@buckeyemail.osu.edu>
2024-04-02 22:12:05 -0400

    uncommented stuff

--- JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
+++ JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
@@ -11,11 +11,11 @@ namespace Jade.Motherboard.Api.Controllers
         [HttpGet]
         public IActionResult GetItems()
         {
-            // var items = new List<Item>()
-            // {
-            //     new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
-            //     new Item("Shorts", "Ohio State shorts.", "Nike", 4499m)
-            // };
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 4499m)
+            };
             return Ok("hello world.");
         }
 
