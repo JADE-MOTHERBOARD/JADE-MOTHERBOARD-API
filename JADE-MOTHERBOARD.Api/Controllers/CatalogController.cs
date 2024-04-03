069d00328b01c476b671c6bc213de31fbb153ba6
Alina Mueez <mueez.1@buckeyemail.osu.edu>
2024-04-02 23:20:33 -0400

    sections 1 thorugh 6 completed

--- JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
+++ JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
@@ -16,8 +16,37 @@ namespace Jade.Motherboard.Api.Controllers
                 new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
                 new Item("Shorts", "Ohio State shorts.", "Nike", 4499m)
             };
-            return Ok("hello world.");
+            return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            return Ok(item);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Item item) {
+            return Created("/catalog/42", item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating) {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+       [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item) {
+            return NoContent();
+        }
+
 
     }
 
--- JADE-MOTHERBOARD.Domain/Catalog/Item.cs
+++ JADE-MOTHERBOARD.Domain/Catalog/Item.cs
@@ -4,6 +4,17 @@ namespace Jade.Motherboard.Domain.Catalog
 {
     public class Item
     {
+        public int Id {get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Brand { get; set; }
+        public decimal Price { get; set; }
+        public List<Rating>? Ratings { get; set; }
+
+        public void AddRating(Rating rating)
+        {
+            this.Ratings.Add(rating);
+        }
         public Item(string name, string description, string brand, decimal price)
         {
             if(string.IsNullOrEmpty(name))
@@ -25,22 +36,11 @@ namespace Jade.Motherboard.Domain.Catalog
                 throw new ArgumentException("Price must be greater than zero.");
             }
 
-            Name = name;
-            Description = description;
-            Brand = brand;
-            Price = price;
+            this.Name = name;
+            this.Description = description;
+            this.Brand = brand;
+            this.Price = price;
         }
 
-        public int Id {get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Brand { get; set; }
-        public decimal Price { get; set; }
-        public List<Rating> Ratings { get; set; } = new List<Rating>();
-
-        public void AddRating(Rating rating)
-        {
-            this.Ratings.Add(rating);
-        }
     }
 }
