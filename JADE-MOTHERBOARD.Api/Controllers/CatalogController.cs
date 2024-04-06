344f6c8043ef11fff1afc35073623b689831a829
Jacob Molina <molina.130@osu.edu>
2024-04-05 20:06:25 -0400

    updating delete

--- JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
+++ JADE-MOTHERBOARD.Api/Controllers/CatalogController.cs
@@ -105,7 +105,15 @@ namespace Jade.Motherboard.Api.Controllers
         
        [HttpDelete("{id:int}")]
         public IActionResult Delete(int id) {
-            return NoContent();
+            var item = _db.Items.Find(id);
+            if (item == null) {
+                return NotFound();
+            }
+
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+
+            return Ok();
         }
 
 
